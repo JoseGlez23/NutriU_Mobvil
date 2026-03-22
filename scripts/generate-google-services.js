d52f4024939d157712e3b590027637184f0a2a85
JoseGlez23 <josegm23c@gmail.com>
2026-03-21 22:21:05 -0700

    Limpieza de archivos sensibles y actualización de configuración segura

--- scripts/generate-google-services.js
+++ scripts/generate-google-services.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const googleServices = process.env.GOOGLE_SERVICES_JSON;
 if (!googleServices) {
-  throw new Error('GOOGLE_SERVICES_JSON env variable not set');
+  throw new Error("GOOGLE_SERVICES_JSON env variable not set");
 }
 
-const filePath = path.join(__dirname, '../android/app/google-services.json');
+const filePath = path.join(__dirname, "../android/app/google-services.json");
 fs.writeFileSync(filePath, googleServices);
-console.log('google-services.json generated!');
+console.log("google-services.json generated!");
